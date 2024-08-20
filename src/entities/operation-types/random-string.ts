5c65833416beab14600613db68d95901df3feab6
Nicolas Carelli <nicolascarelli@gmail.com>
2024-08-20 11:27:41 -0300

    fix: random strings

--- src/entities/operation-types/random-string.ts
+++ src/entities/operation-types/random-string.ts
@@ -7,7 +7,7 @@ export class RandomString extends OperationType {
     super(operations.RANDOM_STRING.cost, operations.RANDOM_STRING.name);
   }
 
-  async calculate(length: number): Promise<string> {
+  async calculate(length: number = 5): Promise<string> {
     const response = await axios.get(
       `https://www.random.org/strings/?num=1&len=${length}&digits=on&upperalpha=on&loweralpha=on&unique=on&format=plain&rnd=new`,
     );
